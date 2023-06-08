48d95213b4e74282ba2e7823206e5e56f4388cc6
Ricky Yang <cyang5075@gmail.com>
2023-06-08 18:08:24 +0800

    main

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,12 +6,23 @@ public class Movie
 {
     public int Id { get; set; }
     [Display(Name = "電影名稱")]
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
     [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [Display(Name = "上映日期")]
     public DateTime ReleaseDate { get; set; }
     [Display(Name = "類型")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
     public string? Genre { get; set; }
     [Column(TypeName = "decimal(18, 2)")]
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
+    [RegularExpression(@"[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string? Rating { get; set; }
 }
